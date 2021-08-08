09c352838822b06a795590d4bd5de8dcf25ef8e0
usakocher <adammkocher@gmail.com>
2021-08-08 09:31:15 -0400

    ready for deployment

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'You shall not pass'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
